d63e34d60900f040165593b4ecf2f9c64f302391
jsvirzi@gmail.com <jsvirzi@gmail.com>
2023-10-14 03:26:16 -0700

    check for out-of-order packets

--- interface/src/stm-interface-test.c
+++ interface/src/stm-interface-test.c
@@ -12,12 +12,17 @@ int main(int argc, char **argv) {
 
     void *stm_stack = NULL; /* use internal stack */
     initialize_stm_interface(stm_stack, port);
+    static uint16_t expected_index = 0;
     while (1) {
         vehicle_data_t vdm[1];
         int status = consume_vehicle_data(stm_stack, vdm);
         if (status > 0) { /* example usage of data */
+            if (expected_index != vdm->index) {
+                printf("*************************************************************************************************** packet out of order\n");
+            }
             printf("$VEHICLE GTIME=%8.8x LTIME=%4.4x TICKS=%8.8x SPEED=%4.4x TIMER=%4.4x CAN_MSGS=%u INDEX=%u*\n",
                 vdm->gtime, vdm->ltime, vdm->ticks, vdm->speed, vdm->timer, vdm->n_can_msgs, vdm->index);
+            expected_index = vdm->index + 1;
         }
     }
     close_stm_interface(stm_stack);
